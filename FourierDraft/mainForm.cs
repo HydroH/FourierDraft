676c64d7ee6bbf68cdd3e4c7fce654e91c48e1b0
HydroH <2322264211@qq.com>
2015-10-13 15:05:20 +0800

    Image processing WIP.

--- FourierDraft/mainForm.cs
+++ FourierDraft/mainForm.cs
@@ -17,20 +17,22 @@ namespace FourierDraft
             InitializeComponent();
         }
 
+        Image img = new Image();
+        Bitmap originBmp = new Bitmap(500, 500);
         private void buttonImportPic_Click(object sender, EventArgs e)
         {
             if (openPic.ShowDialog() == DialogResult.OK)
             {
                 originPicBox.ImageLocation = openPic.FileName;
-                
             }
+            if (null != originPicBox.Image) originBmp = (Bitmap)originPicBox.Image;
         }
 
+        //滚动条与文本框结合
         private void barThreshold_Scroll(object sender, EventArgs e)
         {
             textThreshold.Text = Convert.ToString(barThreshold.Value);
         }
-
         private void textThreshold_TextChanged(object sender, EventArgs e)
         {
             try
@@ -38,6 +40,7 @@ namespace FourierDraft
                 barThreshold.Value = Convert.ToInt32(textThreshold.Text);
             }
             catch { };
+            originPicBox.Image = img.BWConvert(originBmp, barThreshold.Value);
         }
     }
 }

0d392239d41b4eeb49d236ee1b89ca888a8217d5
HydroH <2322264211@qq.com>
2015-10-17 12:03:15 +0800

    Output format small fix.

--- FourierDraft/FourierCurve.cs
+++ FourierDraft/FourierCurve.cs
@@ -58,49 +58,39 @@ namespace FourierDraft
                 curveCoX[i].sin *= 2;
                 curveCoX[i].cos /= period;
                 curveCoX[i].sin /= period;
-                
-                if (curveCoX[i].cos != 0)
-                {
-                    if (curveCoX[i].cos > 0) resultText += " +";
-                    else resultText += " ";
-                    resultText += Convert.ToString(Math.Round(curveCoX[i].cos, 4));
-                    resultText += "cos(" + Convert.ToString(i) + "t)";
-                }
 
-                if (curveCoX[i].sin != 0)
-                {
-                    if (curveCoX[i].sin > 0) resultText += " +";
-                    else resultText += " ";
-                    resultText += Convert.ToString(Math.Round(curveCoX[i].sin, 4));
-                    resultText += "sin(" + Convert.ToString(i) + "t)";
-                }
+                if (Math.Round(curveCoX[i].cos, 4) >= 0) resultText += " +";
+                else resultText += " ";
+                resultText += Convert.ToString(Math.Round(curveCoX[i].cos, 4));
+                resultText += "cos(" + Convert.ToString(i) + "t)";
+
+                if (Math.Round(curveCoX[i].sin, 4) >= 0) resultText += " +";
+                else resultText += " ";
+                resultText += Convert.ToString(Math.Round(curveCoX[i].sin, 4));
+                resultText += "sin(" + Convert.ToString(i) + "t)";
+            
                 resultText += "\r\n";
             }
             
             resultText += "y = ";
             resultText += Convert.ToString(Math.Round(curveCoY[0].cos, 4));
-            for (int i = 1; i <= expansionLevel; i++) 
+            for (int i = 1; i <= expansionLevel; i++)
             {
                 curveCoY[i].cos *= 2;
                 curveCoY[i].sin *= 2;
                 curveCoY[i].cos /= period;
                 curveCoY[i].sin /= period;
 
-                if (curveCoY[i].cos != 0)
-                {
-                    if (curveCoY[i].cos > 0) resultText += " +";
-                    else resultText += " ";
-                    resultText += Convert.ToString(Math.Round(curveCoY[i].cos, 4));
-                    resultText += "cos(" + Convert.ToString(i) + "t)";
-                }
+                if (Math.Round(curveCoY[i].cos, 4) >= 0) resultText += " +";
+                else resultText += " ";
+                resultText += Convert.ToString(Math.Round(curveCoY[i].cos, 4));
+                resultText += "cos(" + Convert.ToString(i) + "t)";
 
-                if (curveCoY[i].sin != 0)
-                {
-                    if (curveCoY[i].sin > 0) resultText += " +";
-                    else resultText += " ";
-                    resultText += Convert.ToString(Math.Round(curveCoY[i].sin, 4));
-                    resultText += "sin(" + Convert.ToString(i) + "t)";
-                }
+                if (Math.Round(curveCoY[i].sin, 4) >= 0) resultText += " +";
+                else resultText += " ";
+                resultText += Convert.ToString(Math.Round(curveCoY[i].sin, 4));
+                resultText += "sin(" + Convert.ToString(i) + "t)";
+            
                 resultText += "\r\n";
             }
             resultText += "\r\n";
